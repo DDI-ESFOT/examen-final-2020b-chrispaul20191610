f78e2405b0254a964be91c52aed3443bc2ffb4c1
chrispaul20191610 <chrispaul20191610.christian.llumiquinga@epn.edu.ec>
2021-03-25 17:54:31 -0500

    estilos antd

--- examen_final/src/App.js
+++ examen_final/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Select, Row, Col } from "antd";
+import 'antd/dist/antd.css';
 import axios from "axios";
 
 function App() {
@@ -21,6 +22,28 @@ function App() {
     });
   };
 
+  const { Option } = Select;
+
+  function onChange(value) {
+    console.log(`selected ${value}`);
+  }
+  
+  function onBlur() {
+    console.log('blur');
+  }
+  
+  function onFocus() {
+    console.log('focus');
+  }
+  
+  function onSearch(val) {
+    console.log('search:', val);
+  }
+
+  function handleMenuClick() {
+    
+  }
+
   return (
     <>
       <div class="container">
@@ -30,16 +53,35 @@ function App() {
 
         <div class="selec">
           <Row justify="center">
-            <Col span={12}>
-              <a>Categorias:</a>
+            <Col span={12} >
+              <p> Categorias:</p>
             </Col>
-            <Col span={12}>
-              
+            <Col span={12} >
+              <Select
+                showSearch
+                style={{ width: 200 }}
+                placeholder="Seleccione una categoria"
+                optionFilterProp="children"
+                onChange={onChange}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                onSearch={onSearch}
+                filterOption={(input, option) =>
+                  option.children.toLowerCase().indexOf(input.toLowerCase()) >=
+                  0
+                }
+              >
+                <Option value=""></Option>
+                <Option value=""></Option>
+                <Option value=""></Option>
+              </Select>
+              ,
             </Col>
           </Row>
 
           <div>
-            <a>{state.joke}</a>
+            <p>{state.joke}</p>
+
           </div>
         </div>
       </div>
