d4460e36f2c9f02e0602d668562c8c557103d787
chrispaul20191610 <chrispaul20191610.christian.llumiquinga@epn.edu.ec>
2021-03-25 17:34:07 -0500

    prettier

--- examen_final/src/App.js
+++ examen_final/src/App.js
@@ -1,60 +1,50 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import { Select,Row, Col } from 'antd';
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Select, Row, Col } from "antd";
+import axios from "axios";
 
 function App() {
   const [state, setState] = useState({
-    joke:''
+    joke: "",
   });
 
   useEffect(() => {
     fetchData();
-    
   }, []);
 
-  const fetchData = async () =>{
-    const result = await axios.get('https://api.chucknorris.io/jokes/random')
-    console.log(result.data.value)
+  const fetchData = async () => {
+    const result = await axios.get("https://api.chucknorris.io/jokes/random");
+    console.log(result.data.value);
     setState({
       ...state,
-      joke:result.data.value
-    })
-  }
-  
-  
+      joke: result.data.value,
+    });
+  };
+
   return (
-    
+    <>
+      <div class="container">
+        <div class="title">
+          <h1>Chuck Norris Jokes</h1>
+        </div>
 
-    <div class="container">
-      <div class="title">
-        <h1>Chuck Norris Jokes</h1>
+        <div class="selec">
+          <Row justify="center">
+            <Col span={12}>
+              <a>Categorias:</a>
+            </Col>
+            <Col span={12}>
+              
+            </Col>
+          </Row>
+
+          <div>
+            <a>{state.joke}</a>
+          </div>
+        </div>
       </div>
-      <div className="selec" >
-        <Row>
-          <Col span={12}>
-            <a>Categorias</a>
-          </Col>
-          <Col span ={12}>
-            <h1>{state.joke}</h1>
-
-          </Col>
-          
-
-        </Row>
-             
-      </div>
-
-      <div>
-
-      </div>
-  
-
-
-    </div>
+    </>
   );
 }
-
 
 export default App;
