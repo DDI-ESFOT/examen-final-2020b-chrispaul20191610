e064fad9bb537982f188d06bd125ed9b21135e55
chrispaul20191610 <chrispaul20191610.christian.llumiquinga@epn.edu.ec>
2021-03-25 17:06:27 -0500

    conexion con el api y broma random

--- examen_final/src/App.js
+++ examen_final/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import { Select,Row, Col } from 'antd';
+import axios from 'axios';
+
 
 function App() {
+  const [state, setState] = useState({
+    joke:''
+  });
+
+  useEffect(() => {
+    fetchData();
+    
+  }, []);
+
+  const fetchData = async () =>{
+    const result = await axios.get('https://api.chucknorris.io/jokes/random')
+    console.log(result.data.value)
+    setState({
+      ...state,
+      joke:result.data.value
+    })
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+    <div class="container">
+      <div class="title">
+        <h1>Chuck Norris Jokes</h1>
+      </div>
+      <div className="selec" >
+        <Row>
+          <Col span={12}>
+            <a>Categorias</a>
+          </Col>
+          <Col span ={12}>
+            <h1>{state.joke}</h1>
+
+          </Col>
+          
+
+        </Row>
+             
+      </div>
+
+      <div>
+
+      </div>
+  
+
+
     </div>
   );
 }
+
 
 export default App;
